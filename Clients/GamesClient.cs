e07c2a865d69fdd9a14c90abbaec63491d0b17b9
lexdeit <godhood@proton.me>
2024-06-01 23:37:47 -0600

    First commit

---
+++ Clients/GamesClient.cs
@@ -0,0 +1,68 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GamesClient
+{
+
+    private readonly List<GameSummary> games =
+   [
+   new(){
+Id = 1,
+Name = "Street Fighter 2",
+Genre = "Fighting",
+Price = 19.99M,
+ReleaseDate = new DateOnly(1992, 7, 15)
+},
+new(){
+Id = 2,
+Name = "Fifa",
+Genre = "Sports",
+Price = 19.99M,
+ReleaseDate = new DateOnly(1992, 7, 15)
+},
+new(){
+Id = 3,
+Name = "Valorant",
+Genre = "Shooter",
+Price = 19.99M,
+ReleaseDate = new DateOnly(1992, 7, 15)
+},
+new(){
+Id = 4,
+Name = "Minecraft",
+Genre = "Kids",
+Price = 19.99M,
+ReleaseDate = new DateOnly(1992, 7, 15)
+},
+new(){
+Id = 5,
+Name = "League Of Legends",
+Genre = "MOBA",
+Price = 19.99M,
+ReleaseDate = new DateOnly(1992, 7, 15)
+}
+   ];
+
+
+    private readonly Genre[] genres = new GenresClient().GetGenres();
+    public GameSummary[] GetGames() => [.. games];
+
+    public void AddGame(GameDetails game)
+    {
+
+        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+        var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+
+        var gameSummary = new GameSummary
+        {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genre.Name,
+            Price = game.Price,
+        };
+
+        games.Add(gameSummary);
+    }
+
+}
---
+++ Clients/GenresClient.cs
@@ -0,0 +1,31 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenresClient
+{
+    private readonly Genre[] genres = [
+        new (){
+            Id= 1,
+            Name= "Fighting",
+        },
+        new (){
+            Id= 2,
+            Name= "Sports",
+        },
+        new (){
+            Id= 3,
+            Name= "Moba",
+        },
+        new (){
+            Id= 4,
+            Name= "Kids & Family",
+        },
+        new (){
+            Id= 5,
+            Name= "Shooter",
+        },
+    ];
+
+    public Genre[] GetGenres() => genres;
+}
