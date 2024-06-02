f0d5dd438333fc2a1488ca224de97819bbcac1f8
lexdeit <godhood@proton.me>
2024-06-02 01:04:20 -0600

    Minor changes

--- Clients/GamesClient.cs
+++ Clients/GamesClient.cs
@@ -50,9 +50,7 @@ ReleaseDate = new DateOnly(1992, 7, 15)
 
     public void AddGame(GameDetails game)
     {
-
-        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
-        var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+        Genre genre = GetGenreById(game.GenreId);
 
         var gameSummary = new GameSummary
         {
@@ -63,6 +61,52 @@ ReleaseDate = new DateOnly(1992, 7, 15)
         };
 
         games.Add(gameSummary);
+    }
+
+    public GameDetails GetGame(int id)
+    {
+        GameSummary game = GetGameSummaryById(id);
+
+        var genre = genres.Single(genre => string.Equals(
+             genre.Name,
+             game.Genre,
+             StringComparison.OrdinalIgnoreCase));
+
+        return new GameDetails
+        {
+            Id = game.Id,
+            Name = game.Name,
+            GenreId = genre.Id.ToString(),
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate,
+        };
+    }
+
+    public void UpdateGame(GameDetails game)
+    {
+        var genre = GetGenreById(game.GenreId);
+
+        GameSummary existingGame = GetGameSummaryById(game.Id);
+
+        existingGame.Name = game.Name;
+        existingGame.Genre = genre.Name;
+        existingGame.Price = game.Price;
+        existingGame.ReleaseDate = game.ReleaseDate;
+
+    }
+
+    private GameSummary GetGameSummaryById(int id)
+    {
+        GameSummary? game = games.Find(game => game.Id == id);
+        ArgumentNullException.ThrowIfNull(game);
+        return game;
+    }
+
+    private Genre GetGenreById(string? id)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(id);
+        return genres.Single(genre => genre.Id == int.Parse(id));
+
     }
 
 }
